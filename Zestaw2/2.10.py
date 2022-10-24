e43c6a6e17407c14b34757712e7b309f017ee4ea
Marek Ostafin <marek.ostafin16@gmail.com>
2022-10-24 08:43:08 +0200

    Dodano zadanie 2.10

---
+++ Zestaw2/2.10.py
@@ -0,0 +1,6 @@
+line = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n" \
+       "Aenean non elementum mi.\tNulla lobortis tincidunt mi.\n" \
+       "Suspendisse quis velit a."
+
+wordcount = len(line.split())
+print(wordcount)
