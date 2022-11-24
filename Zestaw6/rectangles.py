fddba96082a59ff0758e8ddb1fbbfba7802397a3
Marek Ostafin <marek.ostafin16@gmail.com>
2022-11-24 11:48:35 +0100

    Dodano drugą połowę zadania 6.3

--- Zestaw6/rectangles.py
+++ Zestaw6/rectangles.py
@@ -23,19 +23,58 @@ class Rectangle:
         return not self == other
 
     def center(self):           # zwraca środek prostokąta
-        return Point((self.pt2.x - self.pt1.x)/2, (self.pt2.y - self.pt1.y)/2)
+        return Point(self.pt1.x + (self.pt2.x - self.pt1.x)/2, self.pt1.y + (self.pt2.y - self.pt1.y)/2)
 
     def area(self):             # pole powierzchni
         return (self.pt2.x - self.pt1.x) * (self.pt2.y - self.pt1.y)
 
     def move(self, x, y):       # przesunięcie o (x, y)
-        self.pt1.x += x
-        self.pt1.y += y
-        self.pt2.x += x
-        self.pt2.y += y
+        return Rectangle(self.pt1.x + x,
+                         self.pt1.y + y,
+                         self.pt2.x + x,
+                         self.pt2.y + y
+                         )
 
 # Kod testujący moduł.
 
 import unittest
 
-class TestRectangle(unittest.TestCase): pass
+class TestRectangle(unittest.TestCase):
+
+    def test_init(self):
+        t = Rectangle(2, 2, 4, 5)
+        self.assertTrue(t.pt1 == Point(2, 2) and t.pt2 == Point(4, 5))
+
+    def test_str(self):
+        t = Rectangle(2, 2, 4, 5)
+        self.assertEqual(str(t), "[(2, 2), (4, 5)]")
+
+    def test_repr(self):
+        t = Rectangle(2, 2, 4, 5)
+        self.assertEqual(repr(t), "Rectangle(2, 2, 4, 5)")
+
+    def test_eq(self):
+        t1 = Rectangle(2, 2, 4, 5)
+        t2 = Rectangle(2, 2, 4, 5)
+        self.assertTrue(t1 == t2)
+
+    def test_ne(self):
+        t1 = Rectangle(2, 2, 4, 5)
+        t2 = Rectangle(1, 1, 4, 5)
+        self.assertTrue(t1 != t2)
+
+    def test_center(self):
+        t = Rectangle(2, 2, 4, 5)
+        self.assertEqual(t.center(), Point(3, 3.5))
+
+    def test_area(self):
+        t = Rectangle(2, 2, 4, 5)
+        self.assertEqual(t.area(), 6)
+
+    def test_move(self):
+        t = Rectangle(2, 2, 4, 5)
+        self.assertTrue(t.move(1, 1) == Rectangle(3, 3, 5, 6))
+
+
+if __name__ == '__main__':
+        unittest.main()
