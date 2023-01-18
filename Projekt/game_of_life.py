4935f0ba1d0d0d496186868199f0f3c771eb1af9
Marek Ostafin <marek.ostafin16@gmail.com>
2023-01-18 12:30:42 +0100

    Zaludnianie losowych komórek

--- Projekt/game_of_life.py
+++ Projekt/game_of_life.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 import pygame
 import sys
 import numpy as np
@@ -9,6 +12,60 @@ box_size = 25
 BLACK = (0, 0, 0)
 GRAY = (48, 48, 48)
 GREEN = (0, 224, 0)
+
+def drawGrid(grid, boxSize):
+    for x in range(0, len(grid)):
+        for y in range(0, grid.size // len(grid)):
+            if grid[x][y] == 0:
+                # DRAW A BLACK SQUARE
+                rect = pygame.Rect(x * boxSize, y * boxSize, boxSize - 1, boxSize - 1)
+                pygame.draw.rect(screen, BLACK, rect)
+            else:
+                # DRAW A GREEN SQUARE
+                rect = pygame.Rect(x * boxSize, y * boxSize, boxSize - 1, boxSize - 1)
+                pygame.draw.rect(screen, GREEN, rect)
+
+
+def count_neighbors(grid, x, y):
+    neighbors = 0
+    for p in range(x - 1, x + 2):
+        for q in range(y - 1, y + 2):
+            if not (p == x and q == y):
+                if 0 <= p < len(grid) and 0 <= q < (grid.size // len(grid)):
+                    if grid[p][q] == 1:
+                        neighbors += 1
+    return neighbors
+
+
+def life(grid):
+    next_generation = np.zeros((WINDOW_WIDTH // box_size, WINDOW_HEIGHT // box_size))
+    for x in range(0, len(grid)):
+        for y in range(0, grid.size // len(grid)):
+            neighbors = count_neighbors(grid, x, y)
+            if grid[x][y] == 0:  # Komórka jest martwa
+                if neighbors == 3:
+                    next_generation[x][y] = 1
+            else:
+                if neighbors == 2 or neighbors == 3:
+                    next_generation[x][y] = 1
+    return next_generation
+
+def add_random(grid, percentage):
+    num_of_cells_to_add = math.ceil((len(grid) * (grid.size // len(grid))) * percentage/100)
+    timeout = 10
+    while num_of_cells_to_add > 0:
+        test = (grid.size // len(grid))-1
+        x = random.randint(0, len(grid)-1)
+        y = random.randint(0, (grid.size // len(grid))-1)
+        if grid[x][y] == 0:
+            grid[x][y] = 1
+            num_of_cells_to_add -= 1
+            timeout = 10
+        else:
+            timeout -= 1
+
+        if timeout == 0:
+            break
 
 
 def main():
@@ -61,50 +118,14 @@ def main():
                         FPS -= 1
                 elif event.key == pygame.K_c:  # Resetowanie gry
                     grid = np.zeros((WINDOW_WIDTH // box_size, WINDOW_HEIGHT // box_size))
+                elif event.key == pygame.K_r:
+                    add_random(grid, 15) # Po wciśnięciu "R" 15% planszy zostanie zaludnione
 
         if not pause_game:
             grid = life(grid)
 
         pygame.display.update()
         clock.tick(FPS)
-
-
-def drawGrid(grid, boxSize):
-    for x in range(0, len(grid)):
-        for y in range(0, grid.size // len(grid)):
-            if grid[x][y] == 0:
-                # DRAW A BLACK SQUARE
-                rect = pygame.Rect(x * boxSize, y * boxSize, boxSize - 1, boxSize - 1)
-                pygame.draw.rect(screen, BLACK, rect)
-            else:
-                # DRAW A GREEN SQUARE
-                rect = pygame.Rect(x * boxSize, y * boxSize, boxSize - 1, boxSize - 1)
-                pygame.draw.rect(screen, GREEN, rect)
-
-
-def count_neighbors(grid, x, y):
-    neighbors = 0
-    for p in range(x - 1, x + 2):
-        for q in range(y - 1, y + 2):
-            if not (p == x and q == y):
-                if 0 <= p < len(grid) and 0 <= q < (grid.size // len(grid)):
-                    if grid[p][q] == 1:
-                        neighbors += 1
-    return neighbors
-
-
-def life(grid):
-    next_generation = np.zeros((WINDOW_WIDTH // box_size, WINDOW_HEIGHT // box_size))
-    for x in range(0, len(grid)):
-        for y in range(0, grid.size // len(grid)):
-            neighbors = count_neighbors(grid, x, y)
-            if grid[x][y] == 0:  # Komórka jest martwa
-                if neighbors == 3:
-                    next_generation[x][y] = 1
-            else:
-                if neighbors == 2 or neighbors == 3:
-                    next_generation[x][y] = 1
-    return next_generation
 
 
 if __name__ == "__main__":
